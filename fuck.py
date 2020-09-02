f0d2596dd4fa33f58992407e4df91793802c5f04
ruohuali <you@example.com>
2020-09-02 03:18:59 -0500

    auto

---
+++ fuck.py
@@ -0,0 +1,5 @@
+import numpy as np
+x = np.zeros(100)
+for i in range(10):
+	print("fuck"+i+'\r)
+	
---
+++ shcmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#remove old pics
+echo "removing old pics"
+cd eth_plots
+rm **
+cd ..
+
+#start training
+echo "initiating training"
+python3 fuck.py
+echo "training is done"
+
+#git push
+echo "start pushing"
+git add .
+git commit -m "auto"
+git push
+expect "Username for 'https://github.com': "
+send "ruohuali"
+expect "Password for 'https://ruohuali@github.com': "
+send "701218Zhang"
+echo "end pushing"
