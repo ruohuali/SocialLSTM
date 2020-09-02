5d9a5f1b488c1e7e4794271601c649237b56fbdf
ruohuali <ruohualee@instance-1.us-central1-a.c.trysociallstm.internal>
2020-09-02 18:48:13 +0000

    auto

--- VanillaLSTM.py
+++ VanillaLSTM.py
@@ -670,7 +670,7 @@ if __name__ == "__main__":
 #        validate(vl1, 8, 20, file) 
 
 
-    temp = train(8, 20, "datasets/eth/test/biwi_eth.txt")
+    temp = train(8, 20, "datasets/hotel/test/biwi_hotel.txt")
     #validate(temp, 8, 20, "datasets/eth/test/biwi_eth.txt")
     #temp1 = torch.load("model.pt")
     validate(temp, 8, 20, "datasets/hotel/test/biwi_hotel.txt")
