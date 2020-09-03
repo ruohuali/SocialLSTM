3177d81bee994b10145bd8b416719f5347637640
ruohuali <ruohualee@instance-1.us-central1-a.c.trysociallstm.internal>
2020-09-03 09:46:57 +0000

    auto

--- VanillaLSTM.py
+++ VanillaLSTM.py
@@ -287,7 +287,6 @@ def ADE(X, Y):
         x_p, y_p = torch.zeros(2, device=device), torch.zeros(2, device=device)
         for x_off, y_off in zip(x_traj, y_traj):
             x_p += x_off; y_p += y_off
-            if x_off [0] != 0.:
             dist += torch.dist(x_p, y_p)
         traj_dists.append(dist)
     traj_dists = torch.tensor(traj_dists, device=device, requires_grad=True)
