741f5dd72fee06aff570f27d43611692c2c2878b
ruohuali <you@example.com>
2020-09-07 16:08:47 -0500

    auto

--- VanillaLSTM.py
+++ VanillaLSTM.py
@@ -359,7 +359,7 @@ def train(T_obs, T_pred, files, model=None, name="model.pt"):
     for file in files:
         plot_data[file] = [[] for _ in range(60)]
 
-    EPOCH = 100
+    EPOCH = 25
     for epoch in range(EPOCH):
         print(f"epoch {epoch+1}/{EPOCH}  ")
         for file in files:
