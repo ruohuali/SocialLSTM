35681b0979ca16e00dbbe6e3f78121de38952eaa
ruohuali <you@example.com>
2020-09-07 04:28:26 -0500

    auto

--- VanillaLSTM.py
+++ VanillaLSTM.py
@@ -304,13 +304,14 @@ def FDE(X, Y, in_list):
     Y_all = Y.reshape(Y.shape[0]*Y.shape[1],Y.shape[2])    
     Loss = torch.tensor(0. , device=device)
     for traj_idx in in_list:
-        fde = torch.tensor(0. , device=device)
+        fde = torch.tensor(0., device=device)
         traj_mask = torch.tensor([[1.,1.] if i % 20 == traj_idx else [0.,0.] for i in range(X.shape[0]*X.shape[1])], device=device)
         x_traj = X_all*traj_mask
         y_traj = Y_all*traj_mask
-        x_final = torch.sum(x_traj, axis=1)
-        y_final = torch.sum(y_traj, axis=1)
-        fde = torch.dist(x_final, y_final)
+        # x_final = torch.sum(x_traj, axis=1)
+        # y_final = torch.sum(y_traj, axis=1)
+        # fde = torch.dist(x_final, y_final)
+        fde = x_traj+y_traj
         Loss += fde
     return Loss
                 
