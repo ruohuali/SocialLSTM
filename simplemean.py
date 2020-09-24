752683b1f736520721d1ec33c1cfa528289bba2d
ruohuali <ruohualee@instance-1.us-central1-a.c.trysociallstm.internal>
2020-09-24 01:18:28 +0000

    re

--- simplemean.py
+++ simplemean.py
@@ -1,12 +1,16 @@
 import sys
 
-ade_sum = 0.; fde_sum = 0.
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as file:
-        for line in file:
-            ade_sum += line[1]
-            fde_sum += line[2]
-        ade_550_mean = ade_sum/len(file)
-        fde_550_mean = fde_sum/len(file)
+        ade_sum = 0.; fde_sum = 0.
+        for i, l in enumerate (file):
+            line = l.rsplit()
+            if len(line) == 0:
+                continue
+            ade_sum += float(line[1])
+            fde_sum += float(line[2])
+        ade_550_mean = ade_sum/i
+        fde_550_mean = fde_sum/i
+    print(i)
     print(f"mean ade for each entry {ade_550_mean}")
     print(f"mean fde for each entry {fde_550_mean}")    
