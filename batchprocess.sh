eb2576bfba114d79d19f2d8d05fe8aa4d8336bf4
ruohuali <you@example.com>
2020-09-23 22:03:21 -0500

    dissect5

--- batchprocess.sh
+++ batchprocess.sh
@@ -1,11 +1,9 @@
-#!/bin/bash #!/bin/expect
-
-n = $"***********************************************\n\n"
+#!/bin/bash
 
 echo "starting"
 
 for i in {100..156}
 do
    echo "$i * 550"
-   python3 VanillaLSTM.py $i
+   python3 main.py 'v' --special_model 'a_just_trained_model_for_eth.pt' --special_file 'x_all.p' --special_start $2 --T_obs 20 --T_pred 40
 done
